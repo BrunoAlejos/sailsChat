6fc96f20d59c65a370b1178b35c807405f8f2e80
Bruno Alejos <contacto@brunoalejos.info>
2014-05-23 20:19:58 +0200

    first commit

---
+++ api/models/User.js
@@ -0,0 +1,32 @@
+/*jslint node: true */
+'use strict';
+/**
+ *
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+var bcrypt = require('bcrypt');
+
+module.exports = {
+
+    attributes: {
+        username: 'string',
+        password: 'string',
+        loggedIn: 'boolean'
+    },
+
+    beforeCreate: function (values, next) {
+        bcrypt.hash(values.password, 8, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+            values.password = hash;
+            next();
+        });
+    }
+
+};
